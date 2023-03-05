700159ad212c8e0b05b0d6a9e69d6898c661d8f2
yantze <yantze@126.com>
2023-03-05 17:26:03 +0800

    feat: support more action for tumblr

--- tumblr.js
+++ tumblr.js
@@ -1,32 +1,44 @@
-const tumblr = require('tumblr.js');
+module.exports = (RED) => {
+  const tumblr = require('tumblr.js');
 
-function TumblrNode (config) {
-  RED.nodes.createNode(this, config);
+  function TumblrNode (config) {
+    RED.nodes.createNode(this, config);
 
-  const node = this;
+    const node = this;
 
-  const client = tumblr.createClient({
-    consumer_key: config.consumerKey,
-    consumer_secret: config.consumerSecret,
-    token: config.token,
-    token_secret: config.tokenSecret
-  });
-
-  node.on('input', function (msg) {
-    node.status({ fill: "green", shape: "dot", text: "Processing..." });
-    const blogName = (config.blogName || msg.payload.blogName);
-    const postType = (config.postType || 'text');
-
-    client.blogPosts(blogName, { type: postType }, function (err, data) {
-      if (err) {
-        node.status({ fill: "red", shape: "dot", text: "msg.topic is incorrect" });
-        node.error("Error: " + err);
-      } else {
-        node.status({ fill: "blue", shape: "dot", text: "Delete complete" });
-        node.send({ payload: data });
-      }
+    const client = tumblr.createClient({
+      consumer_key: config.consumerKey,
+      consumer_secret: config.consumerSecret,
+      token: config.token,
+      token_secret: config.tokenSecret
     });
-  });
-}
 
-RED.nodes.registerType("tumblr", TumblrNode);
+    node.on('input', function (msg) {
+      node.status({ fill: "green", shape: "dot", text: "Processing..." });
+      const blogName = (config.blogName || msg.payload.blogName);
+      const action = (msg.payload.action || 'createTextPost');
+
+      if (!client[action]) {
+        node.status({ fill: "red", shape: "dot", text: `Unknown actionName: ${action}` });
+        node.error("Error: " + action);
+        return;
+      }
+
+      node.status({ fill: "green", shape: "dot", text: `${action}...` });
+      client[action](blogName, msg.payload.params, function (err, data) {
+        if (err) {
+          node.status({ fill: "red", shape: "dot", text: `${action} fail` });
+          node.error(err);
+          node.send({ error: err });
+        } else {
+          node.status({ fill: "blue", shape: "dot", text: "Post complete" });
+          node.send({ payload: data });
+        }
+      });
+
+    });
+  }
+
+  RED.nodes.registerType("tumblr", TumblrNode);
+
+};
